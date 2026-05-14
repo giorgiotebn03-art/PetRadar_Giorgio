38913c69842da8b59024badd5dee8bc1e105f3c0
giorgiotebn03-art <giorgiotebn03@gmail.com>
2026-05-14 15:06:50 -0600

    Arreglo de Base de datos

--- src/core/db/migrations/1778791862949-InitialMigration.ts
+++ src/core/db/migrations/1778791862949-InitialMigration.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class InitialMigration1778724139737 implements MigrationInterface {
-    name = 'InitialMigration1778724139737'
+export class InitialMigration1778791862949 implements MigrationInterface {
+    name = 'InitialMigration1778791862949'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "found_pets" ("id" SERIAL NOT NULL, "species" character varying NOT NULL, "breed" character varying, "color" character varying NOT NULL, "size" character varying NOT NULL, "description" text NOT NULL, "photo_url" character varying, "finder_name" character varying NOT NULL, "finder_email" character varying NOT NULL, "finder_phone" character varying NOT NULL, "location" geometry(Point,4326) NOT NULL, "address" character varying NOT NULL, "found_date" TIMESTAMP NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_1e8aeb0b37dd97bfce972552b8d" PRIMARY KEY ("id"))`);
